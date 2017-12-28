e26056f48d7bbcfdff1c725e61ebe06ecb81f83d
league <league@wts15.attlocal.net>
2017-12-28 14:32:32 -0800

    stars, butons

--- src/extra/NasaCountdown.java
+++ src/extra/NasaCountdown.java
@@ -3,7 +3,11 @@
 
 package extra;
 
- /* NASA wants to shoot a rocket into orbit. 
+import javax.swing.JOptionPane;
+
+import org.omg.Messaging.SyncScopeHelper;
+
+/* NASA wants to shoot a rocket into orbit. 
   * Somebody has to do the countdown – or the rocket won’t launch.
  * 
  * The ground control chief will tell you where to start – then count down to 0. 
@@ -13,13 +17,23 @@ package extra;
  **/
 
 public class NasaCountdown {
+
 	public static void main(String[] args) {
 		// 2. Use a pop-up to ask the user where to start counting from
-
+String countdown2 = JOptionPane.showInputDialog("What is the countdown?");
+int countdown = Integer.parseInt(countdown2);
 		// 3. Change the countdown to use the new starting point
 		
 		// 1. Print a countdown from 10 to 0 on the console
-
+		for (int i = countdown; i >= 0; i--) {
+		System.out.println("" + i);
+		/*speak("" + i);
+		 * */
+		 
+		if(i == 0) {
+			speak("blast off");
+		}
+		}
 		// 4. Use the speak method to hear the countdown.
 		
 		// 5. when the counting is done, speak "blastoff!"
--- src/extra/SoundtrackToLife.java
+++ src/extra/SoundtrackToLife.java
@@ -11,13 +11,13 @@ public class SoundtrackToLife {
 	public static void main(String[] args) {
 
 		// 1. Adjust this pop-up to find out what mood the user is in.
-		int userMood = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Mood1", "Mood2", "Mood3" }, null);
+		int userMood = JOptionPane.showOptionDialog(null, "What color fisrt comes to your mind?", "Pop-up Title", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Red", "Yellow", "Blue" }, null);
 		
 		// 2. Their answer is stored in the userMood variable. Print it out.
-
+JOptionPane.showMessageDialog(null, "Your mood is	" + userMood);
 		// 3. If they are in a stressed mood, use the playVideo method to play a calming song from YouTube.
-		
+		if
 
 		
 		// 4. Play different songs for other moods.
